83361f4f62674d8e4a7f6482272d2411485b15b4
Johannes Nicolai <jonico@github.com>
2021-09-06 16:17:33 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 
 # always install nektos/act in any GitHub Codespace
 curl https://raw.githubusercontent.com/nektos/act/master/install.sh > install-act.sh
+chmod a+x install-act.sh
 sudo ./install-act.sh v0.2.21
